85ce8860f17bd1f05a5e301a15d4b59376a512c2
Deniska Asus <k.denis.vlad@gmail.com>
2020-08-03 04:03:01 +0200

    анотация

--- src/main/java/it/kurs/SuperServlet.java
+++ src/main/java/it/kurs/SuperServlet.java
@@ -1,13 +1,15 @@
 package it.kurs;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet (name = "SuperServlet")
 public class SuperServlet extends HttpServlet {
     protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.getWriter().println("<h1>Hello, I am SuperServlet!");
+        response.getWriter().println("<h1>Hello, I am SuperServlet!</h1>");
     }
 }
